50ded70b47f53b3ce13b89d39d53203d793ebd45
root <root@maquinadevops.us-central1-a.c.practicadevops.internal>
2020-11-09 17:05:58 +0000

    Buenas

--- hello.py
+++ hello.py
@@ -2,8 +2,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Buenos días  Investigador1'
+def hello():
+    return 'Buenas Buenas Buenas Buenas'
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
